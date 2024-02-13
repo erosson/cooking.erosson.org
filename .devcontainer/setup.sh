053939b4b77c81dd261d478ba9c1844e680044a0
Evan Rosson <git@erosson.org>
2024-02-13 11:02:36 +0000

    gitpod -> devcontainer, netlify -> cf-pages

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+which pandoc || (sudo apt-get update && sudo apt-get install -y pandoc)
--- .gp/before.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env sh
-set -eu
-cd cooking.erosson.org
-cp -n ~/.bashrc ~/.bashrc.original
-cp ~/.bashrc.original ~/.bashrc
-cat .gp/bashrc >> ~/.bashrc
